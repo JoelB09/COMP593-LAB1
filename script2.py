f85884e6369fef1fb245c6013d3f5c314b130a85
JoelB09 <106283010+JoelB09@users.noreply.github.com>
2023-01-25 03:20:40 -0500

    Modified both files in the repo clone
    - Deleted print() statement from script1.py
    - Added one print() statement to scrip2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is easy to explain, it may be a good idea.")
+print("Errors should never pass silently.")
